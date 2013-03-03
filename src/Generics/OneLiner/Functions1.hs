02d8ac2328363b299bedf97e9a7a45b108b24ae7
Sjoerd Visscher <sjoerd@w3future.com>
2013-03-03 19:47:25 +0100

    Added generic functions for kind * -> *.

---
+++ src/Generics/OneLiner/Functions1.hs
@@ -0,0 +1,47 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Generics.OneLiner.Functions1
+-- Copyright   :  (c) Sjoerd Visscher 2013
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  sjoerd@w3future.com
+-- Stability   :  experimental
+-- Portability :  non-portable
+-----------------------------------------------------------------------------
+{-# LANGUAGE RankNTypes, ConstraintKinds, ScopedTypeVariables #-}
+module Generics.OneLiner.Functions1 where
+
+import Generics.OneLiner.ADT1
+import Control.Applicative
+import Control.Monad.Fix
+import Data.Monoid
+import Data.Foldable
+import Data.Traversable
+
+fmapADT :: (ADT1 t, Constraints t Functor) => (a -> b) -> t a -> t b
+fmapADT f ta = builds (For :: For Functor) (\fld -> f (ta ! fld)) (\fld -> fmap f (ta !~ fld)) `at` ta
+
+foldMapADT :: (ADT1 t, Constraints t Foldable, Monoid m) => (a -> m) -> t a -> m
+foldMapADT f ta = mbuilds (For :: For Foldable) (\fld -> f (ta ! fld)) (\fld -> foldMap f (ta !~ fld)) `at` ta
+
+traverseADT :: (ADT1 t, Constraints t Traversable, Applicative f) => (a -> f b) -> t a -> f (t b)
+traverseADT f ta = buildsA (For :: For Traversable) (\fld -> f (ta ! fld)) (\fld -> traverse f (ta !~ fld)) `at` ta
+
+-- unfoldADT :: (ADT1 t, Constraints t Unfoldable, Unfolder f) => f a -> f (t a)
+-- unfoldADT fa = choose $ buildsA (For :: For Unfoldable) (const fa) (const $ unfold fa)
+
+-- | For data types with one constructor
+pureADT :: (ADT1 t, Constraints t Applicative) => a -> t a
+pureADT a = snd $ head $ builds (For :: For Applicative) (const a) (const $ pure a)
+
+-- | For data types with one constructor
+apADT :: (ADT1 t, Constraints t Applicative) => t (a -> b) -> t a -> t b
+apADT tf ta = snd $ head $ builds (For :: For Applicative) (\fld -> (tf ! fld) (ta ! fld)) (\fld -> (tf !~ fld) <*> (ta !~ fld))
+
+-- | For data types with one constructor
+bindADT :: (ADT1 t, Constraints t Monad) => t a -> (a -> t b) -> t b
+bindADT ta f = snd $ head $ builds (For :: For Monad) (\fld -> f (ta ! fld) ! fld) (\fld -> (ta !~ fld) >>= ((!~ fld) . f))
+
+-- | For data types with one constructor
+mfixADT :: (ADT1 t, Constraints t MonadFix) => (a -> t a) -> t a
+mfixADT f = snd $ head $ builds (For :: For MonadFix) (\fld -> fix ((! fld) . f)) (\fld -> mfix ((!~ fld) . f))

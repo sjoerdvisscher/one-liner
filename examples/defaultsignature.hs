40123c6f5b629698512d76ae8fa75a6042630cdd
Sjoerd Visscher <sjoerd@w3future.com>
2014-08-11 23:43:40 +0200

    Converted the examples to use GHC.Generic

--- examples/defaultsignature.hs
+++ examples/defaultsignature.hs
@@ -1,54 +1,36 @@
-{-# LANGUAGE TypeFamilies, DefaultSignatures, ConstraintKinds, TypeOperators #-}
+{-# LANGUAGE DeriveGeneric, DefaultSignatures, ConstraintKinds, TypeOperators, FlexibleContexts #-}
 
-import Generics.OneLiner.ADT
-import Generics.OneLiner.Functions
+import GHC.Generics
+import Generics.OneLiner
 
 import Data.Monoid
-import Control.Applicative
-
-import Text.Read (readPrec)
 
 
 class Size t where
-  
+
   size :: t -> Int
-  
+
   default size :: (ADT t, Constraints t Size) => t -> Int
   size = succ . getSum . gfoldMap (For :: For Size) (Sum . size)
-  
+
 instance Size Bool
 instance Size a => Size (Maybe a)
 
 
 class EnumAll t where
-  
+
   enumAll :: [t]
-  
+
   default enumAll :: (ADT t, Constraints t EnumAll) => [t]
-  enumAll = concat $ buildsA (For :: For EnumAll) (const enumAll)
+  enumAll = concat $ createA (For :: For EnumAll) enumAll
 
 instance EnumAll Bool
 instance EnumAll a => EnumAll (Maybe a)
 
 
 infixr 5 :^:
-data Tree a = Leaf { value :: a } | Tree a :^: Tree a
+data Tree a = Leaf { value :: a } | Tree a :^: Tree a deriving (Show, Generic)
 
-instance ADT (Tree a) where
-  
-  ctorIndex Leaf{}  = 0
-  ctorIndex (_:^:_) = 1
-  ctorInfo _ 0 = CtorInfo "Leaf" True Prefix
-  ctorInfo _ 1 = CtorInfo ":^:"  False (Infix RightAssociative 5)
-  
-  type Constraints (Tree a) c = (c a, c (Tree a))
-  buildsRecA For sub rec = 
-    [ Leaf <$> sub (SelectorInfo "value" value)
-    , (:^:) <$> rec (FieldInfo (\(l :^: _) -> l)) <*> rec (FieldInfo (\(_ :^: r) -> r))
-    ]
-
-instance Show a => Show (Tree a) where showsPrec = showsPrecADT
-instance Read a => Read (Tree a) where readPrec = readPrecADT
 instance Size a => Size (Tree a)
 instance EnumAll a => EnumAll (Tree a)
 
--- examples/paradise.hs
+++ examples/paradise.hs
@@ -1,23 +1,24 @@
-{-# LANGUAGE 
+{-# LANGUAGE
     TypeFamilies
+  , DeriveGeneric
   , ConstraintKinds
+  , FlexibleContexts
   , FlexibleInstances
   , DefaultSignatures
   , OverlappingInstances
-  , TypeSynonymInstances
   #-}
 
-import Generics.OneLiner.ADT
-import Control.Applicative
+import GHC.Generics
+import Generics.OneLiner
 
 
-data Company  = C [Dept]               deriving (Eq, Read, Show)               
-data Dept     = D Name Manager [Unit]  deriving (Eq, Read, Show)
-data Unit     = PU Employee | DU Dept  deriving (Eq, Read, Show)
-data Employee = E Person Salary        deriving (Eq, Read, Show)
-data Person   = P Name Address         deriving (Eq, Read, Show)
-data Salary   = S Float                deriving (Eq, Read, Show)                  
-type Manager  = Employee 
+data Company  = C [Dept]               deriving (Eq, Read, Show, Generic)
+data Dept     = D Name Manager [Unit]  deriving (Eq, Read, Show, Generic)
+data Unit     = PU Employee | DU Dept  deriving (Eq, Read, Show, Generic)
+data Employee = E Person Salary        deriving (Eq, Read, Show, Generic)
+data Person   = P Name Address         deriving (Eq, Read, Show, Generic)
+data Salary   = S Float                deriving (Eq, Read, Show, Generic)
+type Manager  = Employee
 type Name     = String
 type Address  = String
 
@@ -33,46 +34,6 @@ marlow  = E (P "Marlow"  "Cambridge") (S 2000)
 blair   = E (P "Blair"   "London")    (S 100000)
 
 
-instance ADT Company where
-  type Constraints Company c = c [Dept]
-  ctorInfo _ 0 = ctor "C"
-  buildsA For f = [C <$> f (FieldInfo $ \(C l) -> l)]
-
-instance ADT Dept where
-  type Constraints Dept c = (c Name, c Manager, c [Unit])
-  ctorInfo _ 0 = ctor "D"
-  buildsA For f = [D 
-    <$> f (FieldInfo $ \(D n _ _) -> n) 
-    <*> f (FieldInfo $ \(D _ m _) -> m) 
-    <*> f (FieldInfo $ \(D _ _ u) -> u)]
-
-instance ADT Unit where
-  ctorIndex PU{} = 0
-  ctorIndex DU{} = 1
-  ctorInfo _ 0 = ctor "PU"
-  ctorInfo _ 1 = ctor "DU"
-  type Constraints Unit c = (c Employee, c Dept)
-  buildsA For f = 
-    [ PU <$> f (FieldInfo $ \(PU e) -> e)
-    , DU <$> f (FieldInfo $ \(DU d) -> d)
-    ]
-
-instance ADT Employee where
-  type Constraints Employee c = (c Person, c Salary)
-  ctorInfo _ 0 = ctor "E"
-  buildsA For f = [E <$> f (FieldInfo $ \(E p _) -> p) <*> f (FieldInfo $ \(E _ s) -> s)]
-
-instance ADT Person where
-  type Constraints Person c = (c Name, c Address)
-  ctorInfo _ 0 = ctor "P"
-  buildsA For f = [P <$> f (FieldInfo $ \(P n _) -> n) <*> f (FieldInfo $ \(P _ a) -> a)]
-
-instance ADT Salary where
-  type Constraints Salary c = (c Float)
-  ctorInfo _ 0 = ctor "S"
-  buildsA For f = [S <$> f (FieldInfo $ \(S s) -> s)]
-
-  
 class IncreaseSalary t where
   increaseSalary :: Float -> t -> t
   default increaseSalary :: (ADT t, Constraints t IncreaseSalary) => Float -> t -> t
@@ -88,6 +49,6 @@ instance IncreaseSalary Salary where
 instance IncreaseSalary a => IncreaseSalary [a]
 instance IncreaseSalary String where
   increaseSalary _ = id
-  
+
 main :: IO ()
 main = print $ increaseSalary 0.1 genCom

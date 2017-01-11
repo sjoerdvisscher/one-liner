5d9495c3f6485b127563429f5760a9711b697127
Sjoerd Visscher <sjoerd@q42.nl>
2017-01-11 17:45:26 +0100

    Split up ADT and GenericProfunctor classes

--- src/Generics/OneLiner.hs
+++ src/Generics/OneLiner.hs
@@ -28,53 +28,23 @@ module Generics.OneLiner (
   -- * Consuming values
   consume,
   -- * Single constructor functions
-  op0, op1, op2,
+  op0, op1, op2, algebra,
   -- * Generic programming with profunctors
-  GenericProfunctor(..), generic,
+  GenericRecordProfunctor(..), GenericNonEmptyProfunctor(..), GenericProfunctor(..), generic, nonEmpty, record,
   -- * Types
   ADT, ADTRecord, ADTNonEmpty, CtorCount, Constraints, For(..)
 ) where
 
 import GHC.Generics
 import Control.Applicative
+import Data.Bifunctor.Clown
+import Data.Bifunctor.Joker
 import Data.Functor.Identity
-import Data.Functor.Contravariant
+import Data.Functor.Compose
 import Data.Functor.Contravariant.Divisible
 import Data.Profunctor
+import Data.Tagged
 import Generics.OneLiner.Internal
-
-
-newtype Zip f a b = Zip { runZip :: a -> a -> Maybe (f b) }
-instance Functor f => Profunctor (Zip f) where
-  dimap f g (Zip h) = Zip $ \a1 a2 -> fmap (fmap g) (h (f a1) (f a2))
-instance Applicative f => GenericProfunctor (Zip f) where
-  zero = Zip . const $ Just . pure
-  unit = Zip . const $ Just . pure
-  plus (Zip f) (Zip g) = Zip h where
-    h (L1 a) (L1 b) = fmap (fmap L1) (f a b)
-    h (R1 a) (R1 b) = fmap (fmap R1) (g a b)
-    h _ _ = Nothing
-  mult (Zip f) (Zip g) = Zip $ \(al :*: ar) (bl :*: br) -> liftA2 (:*:) <$> f al bl <*> g ar br
-
-newtype Create f a b = Create { unCreate :: [f b] }
-instance Functor f => Profunctor (Create f) where
-  dimap _ f = Create . map (fmap f) . unCreate
-instance Applicative f => GenericProfunctor (Create f) where
-  zero = Create []
-  unit = Create [pure U1]
-  plus (Create l) (Create r) = Create $ map (fmap L1) l ++ map (fmap R1) r
-  mult (Create l) (Create r) = Create $ liftA2 (:*:) <$> l <*> r
-
-newtype Consume f a b = Consume { unConsume :: f a }
-instance Contravariant f => Profunctor (Consume f) where
-  dimap f _ = Consume . contramap f . unConsume
-instance Decidable f => GenericProfunctor (Consume f) where
-  zero = Consume $ lose (\v -> v `seq` undefined)
-  unit = Consume conquer
-  plus (Consume f) (Consume g) = Consume $ choose h f g where
-    h (L1 l) = Left l
-    h (R1 r) = Right r
-  mult (Consume f) (Consume g) = Consume $ divide (\(l :*: r) -> (l, r)) f g
 
 
 -- | Create a value (one for each constructor), given how to construct the components.
@@ -85,7 +55,7 @@ instance Decidable f => GenericProfunctor (Consume f) where
 -- @
 create :: (ADT t, Constraints t c)
        => for c -> (forall s. c s => [s]) -> [t]
-create for f = map runIdentity (createA for (Identity <$> f))
+create for f = runJoker $ generic for $ Joker f
 
 -- | Create a value (one for each constructor), given how to construct the components, under an applicative effect.
 --
@@ -96,19 +66,19 @@ create for f = map runIdentity (createA for (Identity <$> f))
 -- @
 createA :: (ADT t, Constraints t c, Applicative f)
         => for c -> (forall s. c s => [f s]) -> [f t]
-createA for f = unCreate $ generic for (Create f)
+createA for f = getCompose $ runJoker $ generic for $ Joker $ Compose f
 
 -- | Generate ways to consume values of type `t`. This is the contravariant version of `createA`.
 consume :: (ADT t, Constraints t c, Decidable f)
         => for c -> (forall s. c s => f s) -> f t
-consume for f = unConsume $ generic for (Consume f)
+consume for f = runClown $ generic for $ Clown f
 
 
 
 -- | Map over a structure, updating each component.
 gmap :: (ADT t, Constraints t c)
      => for c -> (forall s. c s => s -> s) -> t -> t
-gmap for f = runIdentity . gtraverse for (Identity . f)
+gmap = generic
 
 -- | Map each component of a structure to a monoid, and combine the results.
 --
@@ -124,7 +94,7 @@ gfoldMap for f = getConst . gtraverse for (Const . f)
 -- | Map each component of a structure to an action, evaluate these actions from left to right, and collect the results.
 gtraverse :: (ADT t, Constraints t c, Applicative f)
           => for c -> (forall s. c s => s -> f s) -> t -> f t
-gtraverse for f = runStar $ generic for (Star f)
+gtraverse for f = runStar $ generic for $ Star f
 
 -- | Combine two values by combining each component of the structures with the given function.
 -- Returns `Nothing` if the constructors don't match.
@@ -146,7 +116,22 @@ mzipWith for f l r = maybe mempty getConst $ zipWithA for (\x y -> Just . Const 
 -- Returns `Nothing` if the constructors don't match.
 zipWithA :: (ADT t, Constraints t c, Applicative f)
          => for c -> (forall s. c s => s -> s -> Maybe (f s)) -> t -> t -> Maybe (f t)
-zipWithA for f = runZip $ generic for (Zip f)
+zipWithA for f = runZip $ generic for $ Zip f
+
+newtype Zip f a b = Zip { runZip :: a -> a -> Maybe (f b) }
+instance Functor f => Profunctor (Zip f) where
+  dimap f g (Zip h) = Zip $ \a1 a2 -> fmap (fmap g) (h (f a1) (f a2))
+instance Applicative f => GenericRecordProfunctor (Zip f) where
+  unit = Zip . const $ Just . pure
+  mult (Zip f) (Zip g) = Zip $ \(al :*: ar) (bl :*: br) -> liftA2 (:*:) <$> f al bl <*> g ar br
+instance Applicative f => GenericNonEmptyProfunctor (Zip f) where
+  plus (Zip f) (Zip g) = Zip h where
+    h (L1 a) (L1 b) = fmap (fmap L1) (f a b)
+    h (R1 a) (R1 b) = fmap (fmap R1) (g a b)
+    h _ _ = Nothing
+instance Applicative f => GenericProfunctor (Zip f) where
+  zero = Zip . const $ Just . pure
+
 
 -- | Implement a nullary operator by calling the operator for each component.
 --
@@ -156,7 +141,7 @@ zipWithA for f = runZip $ generic for (Zip f)
 -- @
 op0 :: (ADTRecord t, Constraints t c)
     => for c -> (forall s. c s => s) -> t
-op0 for f = head $ create for [f]
+op0 for f = unTagged $ record for $ Tagged f
 
 -- | Implement a unary operator by calling the operator on the components.
 -- This is here for consistency, it is the same as `gmap`.
@@ -166,7 +151,7 @@ op0 for f = head $ create for [f]
 -- @
 op1 :: (ADTRecord t, Constraints t c)
      => for c -> (forall s. c s => s -> s) -> t -> t
-op1 = gmap
+op1 = record
 
 -- | Implement a binary operator by calling the operator on the components.
 --
@@ -176,6 +161,17 @@ op1 = gmap
 -- @
 op2 :: (ADTRecord t, Constraints t c)
     => for c -> (forall s. c s => s -> s -> s) -> t -> t -> t
-op2 for f l r = case gzipWith for (\a b -> Just (f a b)) l r of
-  Just t -> t
-  Nothing -> error "op2: constructor mismatch should not be possible for ADTRecord"
+op2 for f l r = algebra for (\(Pair a b) -> f a b) (Pair l r)
+
+data Pair a = Pair a a
+instance Functor Pair where
+  fmap f (Pair a b) = Pair (f a) (f b)
+
+-- | Create an F-algebra, given an F-algebra for each of the components.
+--
+-- @
+-- `op2` for f l r = `algebra` for (\\(Pair a b) -> f a b) (Pair l r)
+-- @
+algebra :: (ADTRecord t, Constraints t c, Functor f)
+    => for c -> (forall s. c s => f s -> s) -> f t -> t
+algebra for f = runCostar $ record for $ Costar f
--- src/Generics/OneLiner/Internal.hs
+++ src/Generics/OneLiner/Internal.hs
@@ -25,8 +25,13 @@ module Generics.OneLiner.Internal where
 import GHC.Generics
 import GHC.Types (Constraint)
 import GHC.TypeLits
+import Control.Applicative
+import Data.Bifunctor.Clown
+import Data.Bifunctor.Joker
+import Data.Functor.Contravariant.Divisible
 import Data.Proxy
 import Data.Profunctor
+import Data.Tagged
 
 type family Constraints' (t :: * -> *) (c :: * -> Constraint) :: Constraint
 type instance Constraints' V1 c = ()
@@ -36,64 +41,160 @@ type instance Constraints' (f :*: g) c = (Constraints' f c, Constraints' g c)
 type instance Constraints' (M1 i t f) c = Constraints' f c
 type instance Constraints' (K1 i a) c = c a
 
+type family CtorCount' (t :: * -> *) :: Nat
+type instance CtorCount' V1 = 0
+type instance CtorCount' U1 = 1
+type instance CtorCount' (f :+: g) = CtorCount' f + CtorCount' g
+type instance CtorCount' (f :*: g) = 1
+type instance CtorCount' (M1 i t f) = CtorCount' f
+type instance CtorCount' (K1 i a) = 1
+
 class ADT' (t :: * -> *) where
-  type CtorCount' t :: Nat
-  type CtorCount' t = 1
   ctorIndex' :: t x -> Int
   ctorIndex' _ = 0
   ctorCount :: proxy t -> Int
   ctorCount _ = 1
 
-  p :: (Constraints' t c, GenericProfunctor p)
+  generic' :: (Constraints' t c, GenericProfunctor p)
     => for c -> (forall s. c s => p s s) -> p (t x) (t x)
 
 instance ADT' V1 where
-  type CtorCount' V1 = 0
   ctorCount _ = 0
-  p _ _ = zero
+  generic' _ _ = zero
 
 instance (ADT' f, ADT' g) => ADT' (f :+: g) where
-  type CtorCount' (f :+: g) = CtorCount' f + CtorCount' g
   ctorIndex' (L1 l) = ctorIndex' l
   ctorIndex' (R1 r) = ctorCount (Proxy :: Proxy f) + ctorIndex' r
   ctorCount _ = ctorCount (Proxy :: Proxy f) + ctorCount (Proxy :: Proxy g)
-  p for f = plus (p for f) (p for f)
+  generic' for f = plus (generic' for f) (generic' for f)
 
 instance ADT' U1 where
-  p _ _ = unit
+  generic' _ _ = unit
 
 instance (ADT' f, ADT' g) => ADT' (f :*: g) where
-  p for f = mult (p for f) (p for f)
+  generic' for f = mult (generic' for f) (generic' for f)
 
 instance ADT' (K1 i v) where
-  p _ = dimap unK1 K1
+  generic' _ = dimap unK1 K1
 
 instance ADT' f => ADT' (M1 i t f) where
-  type CtorCount' (M1 i t f) = CtorCount' f
   ctorIndex' = ctorIndex' . unM1
   ctorCount _ = ctorCount (Proxy :: Proxy f)
-  p for f = dimap unM1 M1 (p for f)
+  generic' for f = dimap unM1 M1 (generic' for f)
 
 
-class Profunctor p => GenericProfunctor p where
-  zero :: p (V1 a) (V1 a)
+class (ADT' t) => ADTNonEmpty' (t :: * -> *) where
+  nonEmpty' :: (Constraints' t c, GenericNonEmptyProfunctor p)
+    => for c -> (forall s. c s => p s s) -> p (t x) (t x)
+
+instance (ADTNonEmpty' f, ADTNonEmpty' g) => ADTNonEmpty' (f :+: g) where
+  nonEmpty' for f = plus (nonEmpty' for f) (nonEmpty' for f)
+
+instance ADTNonEmpty' U1 where
+  nonEmpty' _ _ = unit
+
+instance (ADTNonEmpty' f, ADTNonEmpty' g) => ADTNonEmpty' (f :*: g) where
+  nonEmpty' for f = mult (nonEmpty' for f) (nonEmpty' for f)
+
+instance ADTNonEmpty' (K1 i v) where
+  nonEmpty' _ = dimap unK1 K1
+
+instance ADTNonEmpty' f => ADTNonEmpty' (M1 i t f) where
+  nonEmpty' for f = dimap unM1 M1 (nonEmpty' for f)
+
+
+class (ADTNonEmpty' t) => ADTRecord' (t :: * -> *) where
+  record' :: (Constraints' t c, GenericRecordProfunctor p)
+    => for c -> (forall s. c s => p s s) -> p (t x) (t x)
+
+instance ADTRecord' U1 where
+  record' _ _ = unit
+
+instance (ADTRecord' f, ADTRecord' g) => ADTRecord' (f :*: g) where
+  record' for f = mult (record' for f) (record' for f)
+
+instance ADTRecord' (K1 i v) where
+  record' _ = dimap unK1 K1
+
+instance ADTRecord' f => ADTRecord' (M1 i t f) where
+  record' for f = dimap unM1 M1 (record' for f)
+
+
+class Profunctor p => GenericRecordProfunctor p where
   unit :: p (U1 a) (U1 a)
-  plus :: p (f a) (f' a) -> p (g a) (g' a) -> p ((f :+: g) a) ((f' :+: g') a)
   mult :: p (f a) (f' a) -> p (g a) (g' a) -> p ((f :*: g) a) ((f' :*: g') a)
 
-instance Applicative f => GenericProfunctor (Star f) where
-  zero = Star pure
+class GenericRecordProfunctor p => GenericNonEmptyProfunctor p where
+  plus :: p (f a) (f' a) -> p (g a) (g' a) -> p ((f :+: g) a) ((f' :+: g') a)
+
+class GenericNonEmptyProfunctor p => GenericProfunctor p where
+  zero :: p (V1 a) (V1 a)
+
+
+instance GenericRecordProfunctor (->) where
+  unit _ = U1
+  mult f g (l :*: r) = f l :*: g r
+instance GenericNonEmptyProfunctor (->) where
+  plus f _ (L1 l) = L1 (f l)
+  plus _ g (R1 l) = R1 (g l)
+instance GenericProfunctor (->) where
+  zero = id
+
+
+instance GenericRecordProfunctor Tagged where
+  unit = Tagged U1
+  mult (Tagged l) (Tagged r) = Tagged $ l :*: r
+
+
+instance Applicative f => GenericRecordProfunctor (Star f) where
   unit = Star pure
+  mult (Star f) (Star g) = Star $ \(l :*: r) -> (:*:) <$> f l <*> g r
+instance Applicative f => GenericNonEmptyProfunctor (Star f) where
   plus (Star f) (Star g) = Star $ \case
     L1 l -> L1 <$> f l
     R1 r -> R1 <$> g r
-  mult (Star f) (Star g) = Star $ \(l :*: r) -> (:*:) <$> f l <*> g r
+instance Applicative f => GenericProfunctor (Star f) where
+  zero = Star pure
+
+
+instance Functor f => GenericRecordProfunctor (Costar f) where
+  unit = Costar $ const U1
+  mult (Costar f) (Costar g) = Costar $ \lr -> f ((\(l :*: _) -> l) <$> lr) :*: g ((\(_ :*: r) -> r) <$> lr)
+
+
+instance Applicative f => GenericRecordProfunctor (Joker f) where
+  unit = Joker $ pure U1
+  mult (Joker l) (Joker r) = Joker $ (:*:) <$> l <*> r
+instance Alternative f => GenericNonEmptyProfunctor (Joker f) where
+  plus (Joker l) (Joker r) = Joker $ L1 <$> l <|> R1 <$> r
+instance Alternative f => GenericProfunctor (Joker f) where
+  zero = Joker empty
+
+
+instance Divisible f => GenericRecordProfunctor (Clown f) where
+  unit = Clown conquer
+  mult (Clown f) (Clown g) = Clown $ divide (\(l :*: r) -> (l, r)) f g
+instance Decidable f => GenericNonEmptyProfunctor (Clown f) where
+  plus (Clown f) (Clown g) = Clown $ choose h f g where
+    h (L1 l) = Left l
+    h (R1 r) = Right r
+instance Decidable f => GenericProfunctor (Clown f) where
+  zero = Clown $ lose (\v -> v `seq` undefined)
+
 
 -- | All the above functions have been implemented using this single function,
 -- using different `profunctor`s.
 generic :: (ADT t, Constraints t c, GenericProfunctor p)
         => for c -> (forall s. c s => p s s) -> p t t
-generic for f = dimap from to $ p for f
+generic for f = dimap from to $ generic' for f
+
+nonEmpty :: (ADTNonEmpty t, Constraints t c, GenericNonEmptyProfunctor p)
+        => for c -> (forall s. c s => p s s) -> p t t
+nonEmpty for f = dimap from to $ nonEmpty' for f
+
+record :: (ADTRecord t, Constraints t c, GenericRecordProfunctor p)
+        => for c -> (forall s. c s => p s s) -> p t t
+record for f = dimap from to $ record' for f
 
 -- | `Constraints` is a constraint type synonym, containing the constraint requirements for an instance for `t` of class `c`.
 -- It requires an instance of class `c` for each component of `t`.
@@ -109,10 +210,10 @@ type ADT t = (Generic t, ADT' (Rep t))
 type CtorCount t = CtorCount' (Rep t)
 
 -- | `ADTRecord` is a constraint type synonym. An instance is an `ADT` with *exactly* one constructor.
-type ADTRecord t = (ADT t, 1 ~ CtorCount t)
+type ADTRecord t = (Generic t, ADTRecord' (Rep t), 1 ~ CtorCount t)
 
 -- | `ADTNonEmpty` is a constraint type synonym. An instance is an `ADT` with *at least* one constructor.
-type ADTNonEmpty t = (ADT t, 1 <= CtorCount t)
+type ADTNonEmpty t = (Generic t, ADTNonEmpty' (Rep t), 1 <= CtorCount t)
 
 -- | Tell the compiler which class we want to use in the traversal. Should be used like this:
 --

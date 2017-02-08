5eee2d59b4f6e3e6ceb997a1e91c5864a95fcf77
Sjoerd Visscher <sjoerd@w3future.com>
2017-02-08 21:09:21 +0100

    More examples

---
+++ examples/freevars.hs
@@ -0,0 +1,47 @@
+-- Another go at this problem:
+-- https://github.com/sjoerdvisscher/blog/blob/master/2012/2012-03-03%20how%20to%20work%20generically%20with%20mutually%20recursive%20datatypes.md
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances, FlexibleContexts, DeriveGeneric #-}
+
+import GHC.Generics
+import Generics.OneLiner
+
+data Decl = Var := Expr
+          | Seq Decl Decl
+          deriving (Eq, Show, Generic)
+
+data Expr = Con Int
+          | Add Expr Expr
+          | Mul Expr Expr
+          | EVar Var
+          | Let Decl Expr
+          deriving (Eq, Show, Generic)
+
+type Var = String
+
+class Vars t where
+  vars :: t -> [Var] -> ([Var], [Var])
+
+varsDefault :: (ADT t, Constraints t Vars) => t -> [Var] -> ([Var], [Var])
+varsDefault = gfoldMap (For :: For Vars) vars
+
+instance Vars Var where
+  vars v = const ([], [v])
+instance Vars Decl where
+  vars = varsDefault
+instance Vars Int where
+  vars = mempty
+instance Vars Expr where
+  vars (EVar v) = \bound -> (if (v `elem` bound) then [] else [v], [])
+  vars (Let d e) = \bound ->
+    let
+      (freeD, declD) = vars d bound
+      (freeE, _)     = vars e (declD ++ bound)
+    in
+      (freeD ++ freeE, [])
+  vars x = varsDefault x
+
+freeVars :: Vars t => t -> [Var]
+freeVars = fst . ($ []) . vars
+
+test :: [Var]
+test = freeVars $ Let ("x" := Con 42) (Add (EVar "x") (EVar "y"))
---
+++ examples/lenses.hs
@@ -0,0 +1,59 @@
+-- This is a go at creating lenses with one-liner.
+-- It is not a perfect match, but with some unsafeCoerce here and there it works.
+{-# LANGUAGE RankNTypes, TypeOperators, DefaultSignatures, FlexibleContexts, DeriveGeneric, DeriveAnyClass #-}
+import Generics.OneLiner
+import Data.Profunctor
+import GHC.Generics
+import Control.Applicative
+import Unsafe.Coerce (unsafeCoerce)
+
+type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+type Key t = forall x. Lens (t x) (t x) x x
+
+constLens :: x -> Lens s t a b -> x
+constLens x _ = x
+
+index :: f a -> Key f -> a
+index f l = getConst $ l Const f
+
+
+newtype Lensed s t a b = Lensed { getLensed :: Lens s t a b -> b }
+instance Profunctor (Lensed s t) where
+  dimap f g (Lensed ix) = Lensed $ \l -> g (ix (l . (fmap g .) . (. f)))
+instance GenericRecordProfunctor (Lensed s t) where
+  unit = Lensed (constLens U1)
+  mult (Lensed a) (Lensed b) = Lensed (\l -> a (l . fstl) :*: b (l . sndl))
+
+-- GenericRecordProfunctor is a bit too polymorphic,
+-- but we can use unsafeCoerce because the types will end up being the same anyway.
+fstl :: Lens ((a :*: b) x) ((c :*: b') x') (a x) (c x')
+fstl f (a :*: b) = (\c -> c :*: unsafeCoerce b) <$> f a
+sndl :: Lens ((a :*: b) x) ((a' :*: c) x') (b x) (c x')
+sndl f (a :*: b) = (\c -> unsafeCoerce a :*: c) <$> f b
+
+
+class Repr f where
+
+  lensed :: (Lens s t a b -> b) -> Lens s t (f a) (f b) -> f b
+  default lensed :: (ADTRecord1 f, Constraints1 f Repr) => (Lens s t a b -> b) -> Lens s t (f a) (f b) -> f b
+  lensed f = getLensed $ record1 (For :: For Repr) (\(Lensed g) -> Lensed $ lensed g) (Lensed f)
+
+  tabulate :: (Key f -> a) -> f a
+  tabulate f = lensed (\l -> f (runKey (unsafeCoerce (Lens l)))) id
+
+-- Two wrappers needed to make unsafeCoerce happy
+newtype WrappedLens s t a b = Lens { runLens :: Lens s t a b }
+newtype WrappedKey t = Key { runKey :: Key t }
+
+
+data V3 a = V3 a a a deriving (Show, Generic1, Repr)
+data V10 a = V10 a (V3 (V3 a)) deriving (Show, Generic1, Repr)
+
+instance Functor V3 where
+  fmap f v = tabulate (\k -> f (index v k))
+instance Applicative V3 where
+  pure a = tabulate (constLens a)
+  fs <*> as = tabulate (\k -> index fs k (index as k))
+instance Monad V3 where
+  return = pure
+  as >>= f = tabulate (\k -> f (as `index` k) `index` k)
